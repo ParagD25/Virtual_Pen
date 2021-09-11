0f09f2ff387686bd1798bc114dfdf7d8bed29962
Parag <paragdurafe@outlook.com>
2021-09-11 23:10:32 +0530

    Frame added

--- pen.py
+++ pen.py
@@ -1,6 +1,15 @@
 import cv2
 
+fheight=480
+fwidth=640
 capture=cv2.VideoCapture(1)
 
+capture.set(3,fwidth)
+capture.set(4,fheight)
+capture.set(10,150)
+
 while True:
-    break
+    win,img=capture.read()
+    cv2.imshow("Pen",img)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
