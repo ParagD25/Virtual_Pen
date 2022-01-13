e0554d77e185246331521dd5c29f16a272c6bb7c
Parag <paragdurafe@outlook.com>
2022-01-13 23:57:02 +0530

    Color Detection

--- pen.py
+++ pen.py
@@ -5,10 +5,13 @@ capture=cv2.VideoCapture(0)
 
 color=[]
 
-
 def findColors(img):
     imgHSV=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     
+    lower=np.array([h_min,s_min,v_min])
+    upper=np.array([h_max,s_max,v_max])
+
+    mask=cv2.inRange(imgHSV,lower,upper)
 
 def findContours():
     pass
